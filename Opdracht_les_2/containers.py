b15e34bbc27a39cd755f778ab66dfb974bee5bf9
tuwi-0808374 <0808374@hr.nl>
2024-09-04 11:45:14 +0200

    containers opdracht les 2

---
+++ Opdracht_les_2/containers.py
@@ -0,0 +1,12 @@
+unloaded_containers = 331
+unloaded_time_minutes = 441
+
+loaded_containers = 287
+loaded_time_minutes = 295
+
+average_unload_time = unloaded_containers/unloaded_time_minutes
+average_load_time = loaded_containers/loaded_time_minutes
+
+print(f"fDe gemiddelde lostijd bedraagt {average_unload_time} minuten per container")
+print(f"fDe gemiddelde laadtijd bedraagt {average_load_time} minuten per container")
+
