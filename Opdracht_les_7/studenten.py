8f98398d5b3c195130b7881712c312ddc337df3d
tuwi-0808374 <0808374@hr.nl>
2024-09-07 12:40:14 +0200

    best class

--- Opdracht_les_7/studenten.py
+++ Opdracht_les_7/studenten.py
@@ -3,32 +3,40 @@ from students_classrooms import students_per_classroom
 # print(type(students_per_classroom))
 # print(type(students_per_classroom["SWDVT-1A"]))
 
-
-def excellent_students_from_classroom(students):
+def is_student_excellent(student):
     excellent = True
-    excellent_students = []
     for result in student['resultaten']:
         if student['resultaten'][result] == "uitmuntend":
             excellent = True
             break
         if student['resultaten'][result] == "voldoende" or student['resultaten'][result] == "onvoldoende":
             excellent = False
-    if excellent:
-        excellent_students.append(student)
-    return excellent_students
+    return excellent
+
+def get_most_excellent_classroom():
+    classes = []
+    for c_name in students_per_classroom:
+        amount_students = 0
+        amount_ex_students = 0
+        for student in students_per_classroom[c_name]:
+            amount_students += 1
+            if is_student_excellent(student):
+                amount_ex_students += 1
+        classes.append({'klas': c_name, 'aantal_studenten':amount_students, 'aantal_ex_studenten':amount_ex_students})
+    # print(classes)
+    best_class = ("name", 0.0)
+    for cl in classes:
+        percentage = (cl['aantal_ex_studenten'] / cl['aantal_studenten']) * 100
+        if percentage > best_class[1]:
+            best_class = (cl['klas'], percentage)
+        print(f"{cl['klas']}  {percentage}%")
+    print(f"Beste klass is: {best_class[0]}")
 
 for c_name in students_per_classroom:
     for student in students_per_classroom[c_name]:
         # print(student)
         # print(student['naam'])
-        for ex_stu in excellent_students_from_classroom(student):
+        if is_student_excellent(student):
             print(f"Student: {student['naam']} is excellent")
 
-def get_most_excellent_classroom():
-    pass
-
-# def get_excellent_students(students):
-#     excellent_students = []
-#     print(excellent_students)
-#
-# get_excellent_students(students)
+get_most_excellent_classroom()
